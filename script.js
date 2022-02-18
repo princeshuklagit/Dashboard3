e934d6a20b9c2e998c43a6c59bd5340b536b7899
Prince Shukla <princeshuklagithub@gmail.com>
2022-02-18 09:17:28 +0530

    first commite

---
+++ script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     alert("Hello");
+// })
+
+
+$(".profile-setting-panal").hide();
+
+$(".profile-setting").click(function(){
+    $(".profile-setting-panal").show();
+})
